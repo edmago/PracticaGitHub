a2143ce6ab7dddb473562df5945a9df848f82b00
Sugey Useche <sugey.useche@gmail.com>
2012-12-07 16:05:08 -0430

    Modicando menus

--- PracticaGitHub/src/practicagithub/PracticaGitHub.java
+++ PracticaGitHub/src/practicagithub/PracticaGitHub.java
@@ -20,9 +20,11 @@ public class PracticaGitHub {
     public static void main(String[] args) {
         // TODO code application logic here
         clienteGlobal = new cliente[200];
-        int indiceCliente = 0;
         
-        String opcionmod="0",opcionMenu1="0";
+        String opcionmod = "0";
+        
+        InputStreamReader  lector_entrada = new InputStreamReader(System.in);
+        BufferedReader  buffer = new BufferedReader(lector_entrada);
         
         try{
             while(!opcionmod.equals("4"))
@@ -32,15 +34,16 @@ public class PracticaGitHub {
                 System.out.println("    \033[34m2.- \033[39mMódulo Cliente");
                 System.out.println("    \033[34m3.- \033[39mReportes");
                 System.out.println("    \033[34m4.- \033[39mSalir\n");
-                InputStreamReader  lector_entrada = new InputStreamReader(System.in);
-                BufferedReader  buffer = new BufferedReader(lector_entrada);
+                
                 System.out.println("\033[34mIngrese el número de la opción del módulo que desea utilizar: ");
+                
                 opcionmod = buffer.readLine();
                 
                 switch(Integer.parseInt(opcionmod))
                 {
-                    case 1:
+                    case 1: //Menu del módulo administrativo
                     {
+                      String opcionMenu1 = "0";  
                        while(!opcionMenu1.equals("5"))
                         {    
                             System.out.println("\n\n        \033[31mMódulo Administrativo\n");
@@ -50,10 +53,8 @@ public class PracticaGitHub {
                             System.out.println("        \033[31m4.- \033[39mCrear cuenta Bancaria");
                             System.out.println("        \033[31m5.- \033[39mRegresar al menú anterior");
                             System.out.println("        \033[31m6.- \033[39mSalir");
-                            InputStreamReader  lector_entrada2 = new InputStreamReader(System.in);
-                            BufferedReader  buffer2 = new BufferedReader(lector_entrada2);
                             System.out.println("\n\033[34mIngrese el número de la opción del menú que desea utilizar: ");
-                            opcionMenu1 = buffer2.readLine();
+                            opcionMenu1 = buffer.readLine();
                          
                             switch(Integer.parseInt(opcionMenu1))
                             {
@@ -69,13 +70,11 @@ public class PracticaGitHub {
                                 }
                                 case 3:
                                 {                                                                                                            
-                                    InputStreamReader lector_entrada3 = new InputStreamReader(System.in);
-                                    BufferedReader buffer3 = new BufferedReader(lector_entrada3);
                                     System.out.println("Ingrese el Id del Cliente: ");
-                                    long idCliente = Long.parseLong(buffer3.readLine());
+                                    long idCliente = Long.parseLong(buffer.readLine());
                                     clienteGlobal[cliente.indiceCli]= new cliente();
                                     clienteGlobal[cliente.indiceCli].createCliente(idCliente);
-                                    System.out.println("Agregando Cliente");
+                                    System.out.println("Cliente agegado");
                                     break;
                                 }
                                 case 4:
@@ -94,10 +93,90 @@ public class PracticaGitHub {
                             }//fin del switch 2 
                         }//fin del while resp2  
                         break;   
-                    }
-                 }
-            }
-            
+                    }//Fin del case 1 del menu principal 
+                        
+                   case 2: //Menu del módulo de Clientes
+                    {
+                     String opcionMenu2 = "0";     
+                     while(!opcionMenu2.equals("3"))
+                        {
+                            System.out.println("\n\n        \033[31mMódulo Cliente\n");
+                            System.out.println("        \033[31m1.- \033[39mCrear cuenta Bancaria");
+                            System.out.println("        \033[31m2.- \033[39mOperación Bancaria");   
+                            System.out.println("        \033[31m3.- \033[39mRegresar al menú anterior");
+                            System.out.println("        \033[31m4.- \033[39mSalir");
+                            System.out.println("\033[34mIngrese el número de la opción del menú que desea utilizar: ");
+                            opcionMenu2 = buffer.readLine();                       
+                            switch(Integer.parseInt(opcionMenu2))
+                            {
+                                case 1:     
+                                {                                                                         
+                                    System.out.println("Creando Cuenta Bancaria");
+                                    break;
+                                }
+                                case 2:
+                                {
+                                    System.out.println("Operacion Bancaria");
+                                    break; 
+                                }    
+                                case 3:
+                                {                                   
+                                    break; 
+                                }    
+                                case 4:
+                                {
+                                    System.exit(0);
+                                }
+                            }
+                        }
+                        break;
+                   
+                   }//Fin del Case3 del menu principal
+                       
+                   case 3: //Menu de Reportes
+                    {
+                       String opcionMenu3 = "0";
+                       while(!opcionMenu3.equals("7"))
+                        {
+                            System.out.println("\n\n        \033[31mReportes\n");
+                            System.out.println("    \033[31m1.- \033[39mConsulta de Usuario");
+                            System.out.println("    \033[31m2.- \033[39mEstado de Cuenta");
+                            System.out.println("    \033[31m3.- \033[39mListado de Usuarios");
+                            System.out.println("    \033[31m4.- \033[39mListado de Cuentas");
+                            System.out.println("    \033[31m5.- \033[39mListado de Tipo de Operaciones");
+                            System.out.println("    \033[31m6.- \033[39mListado de Tipo de Cuentas");
+                            System.out.println("    \033[31m7.- \033[39mRegresar al menú anterior");
+                            System.out.println("    \033[31m8.- \033[39mSalir");
+                            System.out.println("\033[34mIngrese el número del Reporte que desea visualizar: ");
+                            opcionMenu3 = buffer.readLine();
+                            switch(Integer.parseInt(opcionMenu3))
+                            {
+                                case 1:
+                                {
+                                    break;
+                                }
+                                case 3:
+                                {
+                                    break;
+                                }                                
+                                case 6:
+                                {
+                                    break;
+                                }
+                                case 7:
+                                {
+                                    break;
+                                }
+                                case 8:
+                                {
+                                    System.exit(0);
+                                }                                
+                            }//fin del switch case 3 del menu principal    
+                        }//fin del while opcionMenu3
+                        break;
+                   }//Fin del Case3 del menu principal     
+                 } //Fin del switch principal
+            }//Fin del while principal
         }catch(Exception e)
         {
             
