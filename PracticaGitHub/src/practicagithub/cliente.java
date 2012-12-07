54e638a285680f9af31308dffc95dfd9b2f22f43
Kenny Contreras <kenlis24@gmail.com>
2012-12-07 15:18:36 -0430

    creando cliente

--- PracticaGitHub/src/practicagithub/cliente.java
+++ PracticaGitHub/src/practicagithub/cliente.java
@@ -4,10 +4,27 @@
  */
 package practicagithub;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author Unet
  */
 public class cliente {
+    Long idCliente;
+    String nombreCliente;
+    static int indiceCli = 0;
     
+    public void createCliente(long idcli) throws IOException
+    {
+        idCliente = idcli;
+        InputStreamReader  lector_entrada = new InputStreamReader(System.in);
+        BufferedReader  buffer = new BufferedReader(lector_entrada); 
+        System.out.println("Introduzca el nombre del Cliente: ");
+        String nombre = buffer.readLine();
+        nombreCliente = nombre;
+        indiceCli++;
+    }    
 }
